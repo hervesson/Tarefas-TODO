2aadcde987808ee747de2c1766feefbb6e2198fc
hervesson <hervesson@hotmail.com>
2022-12-16 11:06:35 -0300

    Componente Progresso

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TaskProgress :progress="progress" />
 		<NewTask @taskAdded="addTask" />
 		<TaskGrid 
 			@taskDeleted="deleteTask"
@@ -10,15 +11,24 @@
 </template>
 
 <script>
+
+import TaskProgress from './components/TaskProgress.vue'
 import NewTask from './components/NewTask.vue'
 import TaskGrid from './components/TaskGrid.vue'
 
 
 export default {
-	components: { NewTask, TaskGrid  },
+	components: { TaskProgress, NewTask, TaskGrid  },
 	data() {
 		return {
 			tasks: []
+		}
+	},
+	computed: {
+		progress() {
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
 		}
 	},
 	methods: {
